08c4954ae5bac7ead579d702d25d5034a56d47ea
Bennoama <120720815+Bennoama@users.noreply.github.com>
2024-08-21 21:58:41 +0300

    Removed unused lines of code that was for testing

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import { Telegraf } from 'telegraf';
 import { allowedIDs, botToken } from './environment';
 import { setUpBot } from './setup';
-import axios, { AxiosResponse } from 'axios';
-console.log(botToken);
-console.log(allowedIDs);
 const bot = new Telegraf(botToken);
 
 setUpBot(bot);
