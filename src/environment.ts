275db6f7b4f2bde973573931a5b4fb4afa7d9290
Bennoama <120720815+Bennoama@users.noreply.github.com>
2024-08-23 21:12:57 +0300

    Changed env variables names

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 import env from "env-var";
 
-export const botToken = env.get("botToken").required().asString();
-export const allowedIDs = env.get("allowedIDs").required().asArray();
-export const serverUrl = env.get("serverUrl").required().asString();
+export const botToken = env.get("BOT_TOKEN").required().asString();
+export const allowedIDs = env.get("ALLOWED_IDS").required().asArray();
+export const serverUrl = env.get("TRANSACTION_API_URL").required().asString();
