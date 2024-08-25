17b67285c2d52f7f0967124d02de282b4de80d6c
Bennoama <120720815+Bennoama@users.noreply.github.com>
2024-08-26 00:17:10 +0300

    Fixed configuration for eslint, and env vars

--- src/environment.ts
+++
@@ -1,5 +0,0 @@
-import env from "env-var";
-
-export const botToken = env.get("BOT_TOKEN").required().asString();
-export const allowedIDs = env.get("ALLOWED_IDS").required().asArray();
-export const serverUrl = env.get("TRANSACTION_API_URL").required().asString();
--- src/shared/routeNames.ts
+++
@@ -1,5 +0,0 @@
-export const transactionsInMonthSuffix = "/transactionsInMonth";
-export const statusOfMonthSuffix = "/statusOfMonth";
-export const monthsStatusesSuffix = "/monthsStatuses";
-export const transactionsSinceSuffix = "/transactionsSince";
-export const serverUrl = "http://transaction-api:8080/";
